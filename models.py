278df7b55c8334a3626632c7a25fc0bfcdb5403a
James Morris <github@jmoz.co.uk>
2022-07-16 15:29:45 +0800

    remove candles code

--- models.py
+++ models.py
@@ -1,21 +1,6 @@
 from dataclasses import dataclass, field
-from datetime import datetime, timezone
 
 
 @dataclass
 class Context:
     markets: list = field(default_factory=list)
-    candles: dict = field(default_factory=dict)
-
-
-@dataclass
-class Candle:
-    timestamp: int
-    open: float
-    high: float
-    low: float
-    close: float
-    volume: float
-
-    def dt(self):
-        return datetime.fromtimestamp(self.timestamp, timezone.utc)
